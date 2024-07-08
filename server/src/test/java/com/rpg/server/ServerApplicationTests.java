b778a3339a6e49f083a5ce45a18c5b75ed7519a2
rafabpg <rafa23giordano@gmail.com>
2024-07-08 20:33:33 -0300

    feat: initial commmit

---
+++ server/src/test/java/com/rpg/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpg.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
