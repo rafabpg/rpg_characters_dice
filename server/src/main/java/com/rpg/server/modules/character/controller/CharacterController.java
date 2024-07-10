ae757470297628531394b4da62f2bd50a1112d89
rafabpg <rafa23giordano@gmail.com>
2024-07-10 11:09:55 -0300

    feat: adding mehtod with request params

--- server/src/main/java/com/rpg/server/modules/character/controller/CharacterController.java
+++ server/src/main/java/com/rpg/server/modules/character/controller/CharacterController.java
@@ -8,12 +8,16 @@ import com.rpg.server.modules.character.service.CharacterService;
 
 import jakarta.validation.Valid;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -48,6 +52,46 @@ public class CharacterController {
         }
     }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getById(@PathVariable UUID id) {
+        try {
+            var result = this.characterService.findById(id);
+            return ResponseEntity.status(200).body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error: " + e.getMessage());
+        }
+    }
     
+    @GetMapping("/byClass")
+    public ResponseEntity<Object> getByClass(@RequestParam String characterClass) {
+        try {
+            var result = this.characterService.findByCharacterClass(characterClass);
+            return ResponseEntity.status(200).body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error: " + e.getMessage());
+        }
+    }
+
+
+    @GetMapping("/byRace")
+    public ResponseEntity<Object> getByRace(@RequestParam String race) {
+        try {
+            var result = this.characterService.findByRace(race);
+            return ResponseEntity.status(200).body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteById(@PathVariable UUID id) {
+        try {
+            this.characterService.deleteCharacter(id);
+            return ResponseEntity.status(200).body("Delete Sucessful");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error: " + e.getMessage());
+        }
+    }
+
 
 }
