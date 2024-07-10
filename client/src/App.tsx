281115b2c775b9ffc048a78c20c076dd9b121fa1
rafabpg <rafa23giordano@gmail.com>
2024-07-10 12:18:30 -0300

    feat: adding initial structure to client side

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/routes/routes.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+export const Routes = () => {
+  return (
+
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={} />
+
+        </Routes>
+    </BrowserRouter>
+
+
+  )
+}
